d34582fdf4e0bdc364bf73dbddf4039859410500
Jack_oS <s.panchenko@lookout.com>
2019-12-23 11:47:59 +0200

    refactoring

--- sender.py
+++ sender.py
@@ -8,8 +8,9 @@ logging.basicConfig(filename='send_log', level=logging.INFO, format=format_log)
 
 
 def send():
-    # host = 'http://78.109.28.77/'
-    host = 'http://192.168.0.117:5000/'
+    # host = 'http://192.168.0.117:5000/'
+    host = 'http://78.109.28.77/'
+    # host = 'https://appfarm.org/'
     api = 'fields/api/v01/train_weight'
     storage = 'Фастів ХПП'
     data = wagons()
@@ -34,8 +35,8 @@ def wagons():
     # соединение с локальной БД
     conn_str = (
         r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
-        # r'DBQ=C:\Users\HPP\AppData\Local\VirtualStore\Program Files\TechnowagyLTD\VagonScale\VagonVaga\Database\vagonvaga.mdb;'
-        r'DBQ=\vagonvaga.mdb;'
+        r'DBQ=C:\Users\HPP\AppData\Local\VirtualStore\Program Files\TechnowagyLTD\VagonScale\VagonVaga\Database\vagonvaga.mdb;'
+        # r'DBQ=\vagonvaga.mdb;'
         r'PWD=2708351'
         )
     cnxn = pyodbc.connect(conn_str)
@@ -76,31 +77,7 @@ def wagons():
         wagon = []
         for e in each:
             wagon.append(str(e))
-            # {
-            # 'id_vagon': each[0],
-            # 'brutto': each[1],
-            # 'tara': each[2],
-            # 'netto': each[3],
-            # 'nom_vagon': each[4],
-            #
-            # 'dateb': str(each[5])[:10],
-            # 'timeb': str(each[6])[11:],
-            #
-            # 'rezhym': each[7],
-            # 'nom_poizd': each[8],
-            # 'kil_vagon': each[9],
-            #
-            # 'name_oder': each[10],
-            # 'name_gruz': each[11],
-            # 'name_perev': each[12].replace('"', ''),
-            # 'name_vidpr': each[13].replace('"', '')
-            # }
         data.append(wagon)
-
-    # записать в файл последний отправленный id вагона
-    # if data:
-    #     with open('last_id_vagon', 'w') as file:
-    #         file.write(str(data[-1][0]))
 
     return data
 
