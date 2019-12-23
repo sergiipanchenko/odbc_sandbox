3fee1315080e12374b44c0694ecd5936b19a425b
Jack_oS <s.panchenko@lookout.com>
2019-12-23 10:23:36 +0200

    bug fixed

--- sender.py
+++ sender.py
@@ -8,17 +8,23 @@ logging.basicConfig(filename='send_log', level=logging.INFO, format=format_log)
 
 
 def send():
-    url_api = 'http://78.109.28.77/fields/api/v01/train_weight'
+    # host = 'http://78.109.28.77/'
+    host = 'http://192.168.0.117:5000/'
+    api = 'fields/api/v01/train_weight'
     storage = 'Фастів ХПП'
     data = wagons()
 
     try:
-        response = requests.post(url_api, json={'storage': storage, 'wagons': data})
+        response = requests.post(url=host+api, json={'storage': storage, 'wagons': data})
     except Exception as e:
         logging.error('sending failed (error: ' + str(e) + ')')
     else:
         status = str(response.status_code)
         if status == '200':
+            # записать в файл последний отправленный id вагона
+            if data:
+                with open('last_id_vagon', 'w') as file:
+                    file.write(str(data[-1][0]))
             logging.info('success send ' + str(len(data)) + ' ids (status_code: ' + status + ')')
         else:
             logging.error('sending failed (status_code: ' + status + ')')
@@ -52,47 +58,51 @@ def wagons():
                             gruz,
                             perev,
                             vidpr
-                    WHERE   id_vagon > ? AND id_vagon <= ? AND 
+                    WHERE   id_vagon > ? AND id_vagon <= ? AND
                             cod_main_vagon = id_main_vagon AND
                             cod_oder = id_oder AND
                             cod_gruz = id_gruz AND
                             cod_perev = id_perev AND
                             cod_vidpr = id_vidpr
+                    order by id_vagon ASC;
                     """, (last_id, last_id+1000))
 
     request = crsr.fetchall()
     cnxn.close()
 
-    # упаковка в список словарей для json
-    wagons = []
+    # упаковка для json
+    data = []
     for each in request:
-        # print(each[0])
-        wagons.append({
-            'id_vagon': each[0],
-            'brutto': each[1],
-            'tara': each[2],
-            'netto': each[3],
-            'nom_vagon': each[4],
-
-            'dateb': str(each[5])[:10],
-            'timeb': str(each[6])[11:],
-
-            'rezhym': each[7],
-            'nom_poizd': each[8],
-            'kil_vagon': each[9],
-
-            'name_oder': each[10],
-            'name_gruz': each[11],
-            'name_perev': each[12].replace('"', ''),
-            'name_vidpr': each[13].replace('"', '')
-        })
+        wagon = []
+        for e in each:
+            wagon.append(str(e))
+            # {
+            # 'id_vagon': each[0],
+            # 'brutto': each[1],
+            # 'tara': each[2],
+            # 'netto': each[3],
+            # 'nom_vagon': each[4],
+            #
+            # 'dateb': str(each[5])[:10],
+            # 'timeb': str(each[6])[11:],
+            #
+            # 'rezhym': each[7],
+            # 'nom_poizd': each[8],
+            # 'kil_vagon': each[9],
+            #
+            # 'name_oder': each[10],
+            # 'name_gruz': each[11],
+            # 'name_perev': each[12].replace('"', ''),
+            # 'name_vidpr': each[13].replace('"', '')
+            # }
+        data.append(wagon)
 
     # записать в файл последний отправленный id вагона
-    if wagons:
-        with open('last_id_vagon', 'w') as file:
-            file.write(str(wagons[-1]['id_vagon']))
+    # if data:
+    #     with open('last_id_vagon', 'w') as file:
+    #         file.write(str(data[-1][0]))
 
-    return wagons
+    return data
 
 
 if __name__ == '__main__':
